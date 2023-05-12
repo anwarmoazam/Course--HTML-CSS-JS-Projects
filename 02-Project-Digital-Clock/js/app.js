67e4a9999561baa1196953dab09c46d9ee15a7d7
anwarmoazam <anwar.palimarwar@gmail.com>
2023-05-12 23:28:29 +0530

    project digital clock done

---
+++ 02-Project-Digital-Clock/js/app.js
@@ -0,0 +1,27 @@
+const hoursElement = document.getElementById('hour');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
+let ampmElement = document.getElementById('ampm');
+
+function updateClock(){
+    let hour = new Date().getHours();
+    let minute = new Date().getMinutes();
+    let second = new Date().getSeconds();
+    let ampm = 'AM'
+    if(hour>12){
+        hour = hour-12;
+        ampm = 'PM';
+    }
+    hour = hour<10 ? "0" + hour : hour;
+    minute = minute<10 ? "0" + minute : minute;
+    second = second<10 ? "0" + second : second;
+    
+    hoursElement.innerText = hour;
+    minutesElement.innerText = minute;
+    secondsElement.innerText = second;
+    ampmElement.innerText = ampm;
+    
+    setTimeout(updateClock,1000)
+}
+
+updateClock();
