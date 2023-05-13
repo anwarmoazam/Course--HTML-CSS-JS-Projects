3a3a373ea04eed1a434a428fba45b27eebb7fe6c
anwarmoazam <anwar.palimarwar@gmail.com>
2023-05-13 18:57:29 +0530

    Multiplication app

---
+++ 03-Multiplication-App/js/app.js
@@ -0,0 +1,43 @@
+const questionDOM = document.getElementById('question');
+const buttonDOM = document.querySelector('.btn');
+const scoreDOM = document.getElementById('score');
+const answerDOM = document.getElementById('answer');
+
+
+let num1 = Math.ceil(Math.random()*10);
+let num2 = Math.ceil(Math.random()*10);
+questionDOM.innerText = `What is ${num1} multiply by ${num2}?`;
+let answer = num1 * num2;
+
+let score = JSON.parse(localStorage.getItem('score'));
+scoreDOM.innerText = `score : ${score}`;
+
+if(!score){
+    score =0;
+}
+
+buttonDOM.addEventListener('click',checkAnswer);
+
+function checkAnswer(e){
+    e.preventDefault();
+    let inputAnswer = Number(answerDOM.value);
+    if(answer === inputAnswer){
+        score++;
+        localStorage.setItem('score',JSON.stringify(score));
+        scoreDOM.innerText = `score : ${score}`;
+    } else{
+        score--;
+        localStorage.setItem('score',JSON.stringify(score));
+        scoreDOM.innerText = `score : ${score}`;
+    }
+    answerDOM.value = "";    
+    generateQuestion();
+}
+
+function generateQuestion(){
+    num1 = Math.ceil(Math.random()*10);
+    num2 = Math.ceil(Math.random()*10);
+    questionDOM.innerText = `What is ${num1} multiply by ${num2}?`;
+    answer = num1 * num2;    
+    // return answer;
+}
