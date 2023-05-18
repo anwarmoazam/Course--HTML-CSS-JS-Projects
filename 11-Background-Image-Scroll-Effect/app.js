0769149a0ec2f339eee95eae29edfd8ec93d08e0
anwarmoazam <anwar.palimarwar@gmail.com>
2023-05-18 17:22:18 +0530

    background image scroll effect

--- 11-Background-Image-Scroll-Effect/app.js
+++ 11-Background-Image-Scroll-Effect/app.js
@@ -1,8 +1,8 @@
 const backgroundImage = document.getElementById('bg-image');
 const containerDOM = document.querySelector('.container');
 
-window.addEventListener('scroll',function(event){
-    console.log(`${pageYOffset}%`)
-    // backgroundImage.style.backgroundSize = `${pageYOffset}%`;
-
+window.addEventListener('scroll',function(){
+    let yPosition = window.pageYOffset;
+    backgroundImage.style.opacity = 1-(yPosition/1000);
+    backgroundImage.style.backgroundSize = `${160-(yPosition/12)}%`
 })
